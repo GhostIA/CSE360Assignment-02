55f511b3f18a50993daa6ca8a4c7ae6bb2820833
sarahf5676 <sarahf5676@gmail.com>
2020-10-07 12:04:22 -0700

    Implemented functions

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,37 +2,51 @@ package cse360assignment02;
 // Repository: https://github.com/GhostIA/CSE360Assignment-02
 public class AddingMachine {
     private int total;
+    private String current;
 
     public AddingMachine () {
+        current = "0";
         total = 0;  // not needed - included for clarity
+        /**
+         * Creates values
+         */
     }
 
     public int getTotal () {
-        return 0;
+        return total;
         /***
          * @return total
          */
     }
 
     public void add (int value) {
+        total += value;
+        current = current + " + " + Integer.toString(value);
         /***
-         * @param value
+         * @param value adds to total
          */
     }
 
     public void subtract (int value) {
+        total =- value;
+        current = current + " - " + Integer.toString(value);
         /***
-         * @param value
+         * @param value subtracts from total
          */
     }
 
     public String toString () {
-        return "";
+        return current;
         /***
-         * @return string
+         * @return current
          */
     }
 
     public void clear() {
+        current = "0";
+        total = 0;
+        /**
+         * clears all values
+         */
     }
 }
