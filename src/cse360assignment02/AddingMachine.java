25cf433890031acf05b95bd007d633303cbfc1c5
sarahf5676 <sarahf5676@gmail.com>
2020-10-07 11:54:55 -0700

    Added comments and base classes

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+// Repository: https://github.com/GhostIA/CSE360Assignment-02
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+        /***
+         * @return total
+         */
+    }
+
+    public void add (int value) {
+        /***
+         * @param value
+         */
+    }
+
+    public void subtract (int value) {
+        /***
+         * @param value
+         */
+    }
+
+    public String toString () {
+        return "";
+        /***
+         * @return string
+         */
+    }
+
+    public void clear() {
+    }
+}
